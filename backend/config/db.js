bfec6b5a754a54f2b12e17f3a075916d32e9ebd3
Samad sarwar <samad.s@abhibus.com>
2024-02-27 01:05:09 +0530

    remove colors package

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-const colors = require('colors');
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB Connected : ${conn.connection.host}`.cyan.bold);
+    console.log(`MongoDB Connected : ${conn.connection.host}`);
   } catch (error) {
-    console.log(`Error : ${error.message}`.red.bold);
+    console.log(`Error : ${error.message}`);
     process.exit();
   }
 };
